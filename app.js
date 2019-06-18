6ef940bcbd92d7d06ca602a5215da3bb85ba1895
Yash Kumar Sati <yashsati29@gmail.com>
2019-06-18 07:23:31 +0530

    Completed challenge 11, Added posts arr to store post contents in app.js

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
+let posts = [];
 
 app.set('view engine', 'ejs');
 
@@ -40,11 +41,12 @@ app.get('/compose', function(req, res) {
 })
 
 app.post('/compose', function(req, res) {
-
   const post = {
     title: req.body.postTitle,
     content: req.body.postBody
   }
+  posts.push(post);
+  res.redirect('/');
 })
 
 app.listen(3000, function() {
