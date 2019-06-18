b9a185c7812644ee562f94495166be553c0fc51e
Yash Kumar Sati <yashsati29@gmail.com>
2019-06-18 06:04:19 +0530

    Completed challenge 5, Added same doc structure for about & contact EJS.

--- app.js
+++ app.js
@@ -23,6 +23,18 @@ app.get('/', function(req, res) {
   });
 })
 
+app.get('/about', function(req, res) {
+  res.render('about', {
+    startingContent: aboutContent
+  });
+})
+
+app.get('/contact', function(req, res) {
+  res.render('contact', {
+    startingContent: contactContent
+  });
+})
+
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
