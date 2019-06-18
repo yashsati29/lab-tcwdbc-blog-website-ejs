0701985d79f99f78d4e3cb260cdcf9c16238ffab
Yash Kumar Sati <yashsati29@gmail.com>
2019-06-18 14:48:46 +0530

    Completed challenge 18, post title matching with the url using lodash.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const _ = require('lodash');
 const ejs = require("ejs");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -51,11 +52,10 @@ app.post('/compose', function(req, res) {
 })
 
 app.get('/posts/:postName', function(req, res) {
-  const requestedTitle = req.params.postName;
-  let flag = 0;
+  const requestedTitle = _.lowerCase(req.params.postName);
   posts.forEach(function(post) {
-    const storedTitle = post.title;
-    if (storedTitle.toLowerCase() === requestedTitle) {
+    const storedTitle = _.lowerCase(post.title);
+    if (storedTitle === requestedTitle) {
       console.log('Match Found!');
     }
   })
