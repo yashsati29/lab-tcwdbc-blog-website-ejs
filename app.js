b5cafe8d2a03371841a0d06d7dcf6c2a0d50ed40
Yash Kumar Sati <yashsati29@gmail.com>
2019-06-27 22:52:50 +0530

    Added feature to navigate to created post page.

--- app.js
+++ app.js
@@ -67,14 +67,16 @@ app.post('/compose', function(req, res) {
 
 app.get('/posts/:postName', function(req, res) {
   const requestedTitle = _.lowerCase(req.params.postName);
-  posts.forEach(function(post) {
-    const storedTitle = _.lowerCase(post.title);
-    if (storedTitle === requestedTitle) {
-      res.render('post', {
-        title: post.title,
-        content: post.content
-      });
-    }
+  Post.find({}, function(err, posts) {
+    posts.forEach(function(post) {
+      const storedTitle = _.lowerCase(post.title);
+      if (storedTitle === requestedTitle) {
+        res.render('post', {
+          title: post.title,
+          content: post.content
+        });
+      }
+    })
   })
 });
 
